6353d28af9f3fdaa2e76ffcb17905620fe959cc9
voidGlitch <official.shreshthav@gmail.com>
2024-10-20 20:37:20 +0530

    added location

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,54 +7,63 @@ const images = [
     title: "Kashmir",
     description:
       "A picturesque region known for its stunning landscapes, lush valleys, and vibrant culture.",
+    loc: "Jammu and Kashmir, India",
   },
   {
     src: "/kheer.jpg",
     title: "Kheerganga",
     description:
       "A scenic trekking destination famous for its hot springs and breathtaking views of the Himalayas.",
+    loc: "Himachal Pradesh, India",
   },
   {
     src: "/Taj.jpg",
     title: "Taj Mahal",
     description:
       "An iconic white marble mausoleum, symbolizing love, and one of the Seven Wonders of the World.",
+    loc: "Uttar Pradesh, India",
   },
   {
     src: "/IndiaGate.jpg",
     title: "India Gate",
     description:
       "A monumental archway in New Delhi, honoring the soldiers who died in World War I.",
+    loc: "Delhi, India",
   },
   {
     src: "/delhi2.jpg",
     title: "Red Fort",
     description:
       "A UNESCO World Heritage Site, this historic fort represents Mughal architecture in Delhi.",
+    loc: "Delhi, India",
   },
   {
     src: "/Prem.jpg",
     title: "Prem Mandir",
     description:
       "A beautiful temple in Vrindavan, known for its intricate carvings and vibrant devotional atmosphere.",
+    loc: "Uttar Pradesh, India",
   },
   {
     src: "/fort.jpg",
     title: "Jaipur Fort",
     description:
       "A majestic fort in the Pink City, showcasing Rajasthan's royal heritage and architectural grandeur.",
+    loc: "Rajasthan, India",
   },
   {
     src: "/lotus.jpg",
     title: "Lotus Temple",
     description:
       "A Bahá'í House of Worship in Delhi, celebrated for its flower-like design and serene ambiance.",
+    loc: "Delhi, India",
   },
   {
     src: "/vara.webp",
     title: "Varanasi",
     description:
       "One of the oldest cities in the world, renowned for its spiritual significance and ghats along the Ganges River.",
+    loc: "Uttar Pradesh, India",
   },
 ];
 
@@ -85,7 +94,7 @@ const Slider = () => {
           >
             <img src={image.src} alt={image.title} />
             <div className="content">
-              <p>{image.title}</p>
+              <p>{image.loc}</p>
               <h2>{image.title}</h2>
               <p>{image.description}</p>
             </div>
